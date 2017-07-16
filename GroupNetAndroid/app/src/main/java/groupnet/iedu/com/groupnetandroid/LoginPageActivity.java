66f38f367aba1ddca4175f26ff3e6a0059714dae
taeingu <gupapa999@gmail.com>
2017-07-16 16:48:37 -0700

    update

--- GroupNetAndroid/app/src/main/java/groupnet/iedu/com/groupnetandroid/LoginPageActivity.java
+++ GroupNetAndroid/app/src/main/java/groupnet/iedu/com/groupnetandroid/LoginPageActivity.java
@@ -1,7 +1,10 @@
 package groupnet.iedu.com.groupnetandroid;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class LoginPageActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class LoginPageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
+    }
+    public final static String EXTRA_MESSAGE = "com.mycompany.myfirstapp.MESSAGE";
+    public void login (View view) {
+        Intent intent = new Intent(this, MainActivity .class);
+        //EditText editText = (EditText) findViewById(R.id.edit_message);
+        //String message = editText.getText().toString();
+        //intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
