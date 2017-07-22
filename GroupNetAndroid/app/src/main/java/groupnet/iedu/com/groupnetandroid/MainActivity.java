e76aefa6a8869b77fd100f70c6f992b11366e3c5
mhlee <mhlee1215@gmail.com>
2017-07-21 21:10:31 -0700

    network connection for android added

--- GroupNetAndroid/app/src/main/java/groupnet/iedu/com/groupnetandroid/MainActivity.java
+++ GroupNetAndroid/app/src/main/java/groupnet/iedu/com/groupnetandroid/MainActivity.java
@@ -5,6 +5,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.iedu.client.*;
+import com.iedu.domain.Group;
+
+import groupnet.iedu.com.groupnetandroid.samples.connection.ConnectionSample;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -13,17 +18,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void login(View view) {
-        System.out.print("logged in");
+    public void readGroupInfo(View view){
+        System.out.println("Run Connection!");
+        ConnectionSample cs = new ConnectionSample();
+        //This will call doInBackground() function
+        cs.execute("param1", "param2", "param3");
     }
 
-    public void createAccount(View view){
-        System.out.print("creating account");
-    }
-
-    public void profileOne (View view){
-        System.out.print("profile page 1");
-    }
-    Intent intent = getIntent();
-    //String message = intent.getStringExtra(LoginPageActivity.EXTRA_MESSAGE);
 }
---
+++ GroupNetAndroid/app/src/main/java/groupnet/iedu/com/groupnetandroid/Utils/Logger.java
@@ -0,0 +1,11 @@
+package groupnet.iedu.com.groupnetandroid.Utils;
+
+/**
+ * Created by mhlee on 7/21/17.
+ */
+
+public class Logger {
+    public static void log(String str){
+        System.out.println("GroupNet::"+str);
+    }
+}
