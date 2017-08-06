d387ce82b0620c9e4dae4e84ef7705e7a08f87cd
mhlee <mhlee1215@gmail.com>
2017-08-06 14:39:27 -0700

    login id pull

---
+++ GroupNetAndroid/app/src/main/java/groupnet/iedu/com/groupnetandroid/Connections/ConnectionLogin.java
@@ -0,0 +1,61 @@
+package groupnet.iedu.com.groupnetandroid.Connections;
+
+import android.content.Context;
+import android.os.AsyncTask;
+
+import com.iedu.client.GroupClient;
+import com.iedu.client.UserClient;
+import com.iedu.domain.Group;
+import com.iedu.domain.Signal;
+import com.iedu.domain.User;
+
+import java.util.List;
+
+import groupnet.iedu.com.groupnetandroid.LoginPageActivity;
+import groupnet.iedu.com.groupnetandroid.samples.components.SampleActivity;
+
+/**
+ * Created by mhlee on 7/21/17.
+ */
+
+public class ConnectionLogin extends AsyncTask {
+
+    private LoginPageActivity context;
+    int returnCode;
+
+    public ConnectionLogin(Context context){
+        this.context = (LoginPageActivity)context;
+    }
+
+    //This function will be called after call execution()
+    @Override
+    protected Object doInBackground(Object... arg0) {
+
+        try {
+            User user = new User();
+            user.setName((String)arg0[0]); // user name
+            user.setPassword((String)arg0[1]); // user password
+            returnCode = UserClient.login(user);
+
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(Object o) {
+        super.onPostExecute(o);
+
+        //If login success
+        if( returnCode == Signal.LOGIN_SUCCESS) {
+            context.loginSuccess();
+        }
+        else{
+            context.loginFail();
+        }
+
+
+    }
+}
--- GroupNetAndroid/app/src/main/java/groupnet/iedu/com/groupnetandroid/LoginPageActivity.java
+++ GroupNetAndroid/app/src/main/java/groupnet/iedu/com/groupnetandroid/LoginPageActivity.java
@@ -4,7 +4,9 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
+import groupnet.iedu.com.groupnetandroid.Connections.ConnectionLogin;
 import groupnet.iedu.com.groupnetandroid.samples.components.SampleActivity;
 
 public class LoginPageActivity extends AppCompatActivity {
@@ -16,10 +18,30 @@ public class LoginPageActivity extends AppCompatActivity {
     }
     //public final static String EXTRA_MESSAGE = "com.mycompany.myfirstapp.MESSAGE";
     public void loginButton (View view) {
+
+        EditText userId = (EditText)findViewById(R.id.user_id);
+        System.out.println(userId.getText());
+
+        EditText userPassword = (EditText)findViewById(R.id.user_password);
+        System.out.println(userPassword.getText());
+
+        ConnectionLogin cl = new ConnectionLogin(this);
+
+        cl.execute("user", "password");
+
+
         Intent intent = new Intent(this, SampleActivity.class);
         //EditText editText = (EditText) findViewById(R.id.edit_message);
         //String message = editText.getText().toString();
         //intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
+    }
+
+    public void loginSuccess(){
+        System.out.println("success");
+    }
+
+    public void loginFail(){
+        System.out.println("fail");
     }
 }
