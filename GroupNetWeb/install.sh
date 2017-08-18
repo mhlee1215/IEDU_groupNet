503a735e1e108c1eb9cd9832d8bc05acf5596994
Ubuntu <ubuntu@ip-172-31-42-71.us-west-2.compute.internal>
2017-08-18 23:43:29 +0000

    install updated

--- GroupNetWeb/install.sh
+++ GroupNetWeb/install.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
-rm -rf ~/tomcat_home/webapps/think-forum-web*
-cp ./target/think-forum-web.war ~/tomcat_home/webapps/
 ~/tomcat_home/bin/shutdown.sh
+
+git pull
+
+cd ../GroupNetCore
+ant
+cd ../GroupNetWeb
+
+mvn package
+
+rm -rf ~/tomcat_home/webapps/GroupNetWeb*
+cp ./target/GroupNetWeb.war ~/tomcat_home/webapps/
 ~/tomcat_home/bin/startup.sh
+
+tail -f ~/tomcat_home/logs/catalina.out
