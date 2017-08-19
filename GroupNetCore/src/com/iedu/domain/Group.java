84b338053bb2f9a9ea26ed298a97dfc333f17e46
mhlee <mhlee1215@gmail.com>
2017-08-18 17:06:18 -0700

    group domain fixed

--- GroupNetCore/src/com/iedu/domain/Group.java
+++ GroupNetCore/src/com/iedu/domain/Group.java
@@ -9,49 +9,8 @@ public class Group {
 	String url = "";
 	String url2 = ""; 
 	//{"${member.name()}":"${member.value}", "${otherMembers}"}
-	public int getId() { 
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getDescription() {
-		return description;
-	}
-	public void setDescription(String description) {
-		this.description = description;
-	}
-	public String getStatus() {
-		return status;
-	}
-	public void setStatus(String status) {
-		this.status = status;
-	}
-	public String getRegistration_date() {
-		return registration_date;
-	}
-	public void setRegistration_date(String registration_date) {
-		this.registration_date = registration_date;
-	}
-	public String getUrl() {
-		return status;
-	}
-	public void setUrl(String url) {
-		this.url = url;
-		//this.url = "111";
-	}
-	public String getUrl2() {
-		return url2;
-	}
-	public void setUrl2(String url2) {
-		this.url2 = url2;
-	}
+	
+	
 	
 	@Override
 	public String toString() {
@@ -60,6 +19,62 @@ public class Group {
 				+ url + "\", \"url2\":\"" + url2 + "\"}";
 	}
 	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getRegistration_date() {
+		return registration_date;
+	}
+
+	public void setRegistration_date(String registration_date) {
+		this.registration_date = registration_date;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public String getUrl2() {
+		return url2;
+	}
+
+	public void setUrl2(String url2) {
+		this.url2 = url2;
+	}
+
 	public static void main(String[] argv){
 		Group g = new Group();
 		System.out.println(g);
